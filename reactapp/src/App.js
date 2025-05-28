e524682c893fea53e82fcc55e75b8089d6cc0a63
nive1712 <nivesundaramoorthy@gmail.com>
2025-05-28 11:55:00 +0000

    frontend code commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
-import Home from "./Components/HomePage";
+
 import ViewAllAccountsComponent from "./ManagerComponents/ViewAllAccountsComponent";
 function App(){
     return (
         <Router>
             <Routes>
-                <Home/>
+             
                 <Route path="/manager/accounts" element={<ViewAllAccountsComponent/>}/>
             </Routes>
         </Router>
