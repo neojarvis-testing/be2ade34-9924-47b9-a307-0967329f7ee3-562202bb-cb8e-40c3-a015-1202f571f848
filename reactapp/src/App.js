71463973b29ed5ddd7d29044ed3ab4a1d529fb7d
nive1712 <nivesundaramoorthy@gmail.com>
2025-05-28 15:24:37 +0000

    frontend code

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 
-import ViewAllAccountsComponent from "./ManagerComponents/ViewAllAccountsComponent";
 function App(){
     return (
         <Router>
             <Routes>
              
-                <Route path="/manager/accounts" element={<ViewAllAccountsComponent/>}/>
+                <Route path="/" element ={<div>wlemome!</div>}/>
             </Routes>
         </Router>
     );
